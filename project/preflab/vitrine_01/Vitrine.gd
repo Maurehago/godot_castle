f9be7b4c969765735d80116b56dac92248d2d705
Holger Winter <winter@tuhh.de>
2019-09-09 00:15:02 +0200

    Vitrine eingebunden

---
+++ project/preflab/vitrine_01/Vitrine.gd
@@ -0,0 +1,30 @@
+extends Spatial
+
+export var Szene_01 = ""
+export var Szene_02 = ""
+export var Szene_03 = ""
+export var Bild = ""
+
+func _ready():
+	
+	$vitrine/Sprite3D.texture = load(Bild)
+	set_process(false)
+	pass
+
+func _process(delta):
+	if Input.is_action_pressed("vitrine_1"):
+		get_node("/root/AutoLoad").zu_nebenszene(Szene_01)
+		set_process(false)
+	if Input.is_action_pressed("vitrine_2"):
+		get_node("/root/AutoLoad").zu_nebenszene(Szene_02)
+		set_process(false)
+	if Input.is_action_pressed("vitrine_3"):
+		get_node("/root/AutoLoad").zu_nebenszene(Szene_03)
+		set_process(false)
+		
+
+func _on_Area_body_entered(body):
+	set_process(true)
+
+func _on_Area_body_exited(body):
+	set_process(false)
