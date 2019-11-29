2863ff2bf5c6ab65d3f0d1f323432183bcf7e7d5
maurehago <romhub@aon.at>
2019-11-29 19:53:30 +0100

    Autoload Skript angepasst
    damit der Player beim Szenenwechsel auf Ppsition bleibt

--- project/AutoLoad.gd
+++ project/AutoLoad.gd
@@ -1,7 +1,20 @@
 extends Node
 
+#sceneTree
+var sceneTree :SceneTree
+
+# Einstellungen für die Hauptszene
+var HauptSzenePath : String  = "res://szene/Burg1.tscn"
+var HauptSzene : Node
+var packedHauptSzene : PackedScene
+var isHauptSzene : bool = false
+
+# Spieler Position
+var playerPos : Transform
+var playerPath : String = "/root/Burg1/Burg/Player"
+
+# Weitere Einstellungen
 var NebenSzene : String
-var HauptSzene : String  = "res://szene/Burg1.tscn"
 var SzeneReader : String = "res://preflab/SzeneReader/SzeneReader.tscn"
 var my_pos : Vector3
 var my_richtung : Vector3
@@ -10,22 +23,60 @@ var SzeneReaderAktiv : bool = true
 
 func _ready():
 	set_process_input(false)
+	
+	# SzeneTree merken
+	sceneTree = get_tree()
+	
+	# Hauptszene merken
+	HauptSzene = sceneTree.current_scene
+	isHauptSzene = true
+
+func zu_hauptszene():
+	# Aktuelle Nebenszene auslesen und entfernen
+	var current : Node = sceneTree.current_scene
+	sceneTree.root.remove_child(current)
+	current.queue_free()
+	
+	# Hauptszene neu setzen
+	HauptSzene = packedHauptSzene.instance()
+	sceneTree.root.add_child(HauptSzene)
+	isHauptSzene = true
+	set_process_input(false)
+	
+	# Player Position neu
+	get_node(playerPath).global_transform = playerPos
 
 func zu_nebenszene(weg):
+	# Nebenszene aufrufen
 	NebenSzene = weg
 	if DummiFile.file_exists(NebenSzene):
 		set_process_input(true)
-		get_tree().change_scene(NebenSzene)
+		
+		# wenn Hauptszene
+		if isHauptSzene:
+			# Position merken
+			playerPos = get_node(playerPath).global_transform
+			
+			# Hauptszene wegpacken
+			packedHauptSzene = PackedScene.new()
+			packedHauptSzene.pack(HauptSzene)
+			sceneTree.root.remove_child(HauptSzene)
+			HauptSzene.queue_free()
+
+		sceneTree.change_scene(NebenSzene)
+		isHauptSzene = false
 
 func _input(event):
 	if event.is_action_pressed("ui_focus_next"):
 		print(SzeneReaderAktiv)
 		if SzeneReaderAktiv:
 			SzeneReaderAktiv = false
-			get_tree().change_scene(SzeneReader)
+			sceneTree.change_scene(SzeneReader)
 		else:
 			SzeneReaderAktiv = true
-			get_tree().change_scene(NebenSzene)
+			sceneTree.change_scene(NebenSzene)
+	
+	# wenn Abbruch
 	if event.is_action_pressed("ui_end"):
-		set_process_input(false)
-		get_tree().change_scene(HauptSzene)
+		# zur Hauptszene wechseln
+		zu_hauptszene()
