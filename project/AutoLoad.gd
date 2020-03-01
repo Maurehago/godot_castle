29f806532a037ec3de183ce0ab98c656ad4fdd3c
maurehago <romhub@aon.at>
2020-03-01 13:55:23 +0100

    Anpassung im Autoload

--- project/AutoLoad.gd
+++ project/AutoLoad.gd
@@ -21,48 +21,66 @@ var my_richtung : Vector3
 var DummiFile =File.new();
 var SzeneReaderAktiv : bool = true
 
+var Main :Node
+
 func _ready():
 	set_process_input(false)
 	
 	# SzeneTree merken
 	sceneTree = get_tree()
+
+	# hier für den Zugriff auf das Main
+	Main = sceneTree.get_root().get_node("Main")
 	
 	# Hauptszene merken
-	HauptSzene = sceneTree.current_scene
+	# alt: HauptSzene = sceneTree.current_scene
+	#neu: Merken der Main Hauptszene
+	HauptSzene = Main
 	isHauptSzene = true
 
 func zu_hauptszene():
 	# Aktuelle Nebenszene auslesen und entfernen
 	var current : Node = sceneTree.current_scene
+	# Fehler bei Ausstieg
+	if !current:
+		return
+	print (current.get_path())
 	sceneTree.root.remove_child(current)
 	current.queue_free()
+
+	# alt:set_process_input(false)
+	# neu: Maus verstecken
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 	# Hauptszene neu setzen
-	HauptSzene = packedHauptSzene.instance()
+	# alt: HauptSzene = packedHauptSzene.instance()
 	sceneTree.root.add_child(HauptSzene)
 	isHauptSzene = true
-	set_process_input(false)
+	
+	# LOD's neu Prüfen
+	Main.Player.LOD_init = true
+
 	
 	# Player Position neu
-	get_node(playerPath).global_transform = playerPos
+	# alt: get_node(playerPath).global_transform = playerPos
 
 func zu_nebenszene(weg):
 	# Nebenszene aufrufen
 	NebenSzene = weg
 	if DummiFile.file_exists(NebenSzene):
-		set_process_input(true)
-		
 		# wenn Hauptszene
 		if isHauptSzene:
 			# Position merken
-			playerPos = get_node(playerPath).global_transform
+			# alt: playerPos = get_node(playerPath).global_transform
 			
 			# Hauptszene wegpacken
-			packedHauptSzene = PackedScene.new()
-			packedHauptSzene.pack(HauptSzene)
+			# alt: packedHauptSzene = PackedScene.new()
+			# alt:packedHauptSzene.pack(HauptSzene)
 			sceneTree.root.remove_child(HauptSzene)
-			HauptSzene.queue_free()
-
+			# auf keinem Fall die Hauptszene löschen, da der Player da schon positioniert ist
+			# alt: HauptSzene.queue_free()
+			
+		set_process_input(true)
 		sceneTree.change_scene(NebenSzene)
 		isHauptSzene = false
 
